276efd4c72964f44bf9b8f883b78e447ba5cb550
unknown <yhlee89.dho@gmail.com>
2019-12-05 19:41:12 -0600

    get device coords

---
+++ script.js
@@ -0,0 +1,23 @@
+// Get User device location on click of startBtn
+$("#startBtn").on("click", function(start) {
+    getLocation()
+})
+
+// Check if browser allows geolocation
+function getLocation() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else {
+        alert("Use a different browser")
+    }
+}
+
+// Get coords from geolocation
+function showPosition(position) {
+    let lat = postion.coords.latitude;
+    let lon = position.coords.longitude;
+    console.log("Device Location Latitude: " + lat + " Longitude: " + lon);
+    redirect(lat, lon);
+}
+

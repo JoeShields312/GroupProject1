062c5ba3714c3ad09e198bd4c2f0cf4d1ff2d882
Jen C <jenjch8@gmail.com>
2019-12-09 21:18:29 -0600

    added OMDB call and output to container. Commented steps. Additions to CSS

--- movieglue.js
+++ movieglue.js
@@ -1,3 +1,4 @@
+// Click to Display button triggers the showing of coordinates and all the cascading API calls (also controlled by click events) below
 $("#startBtn").on("click", function() {
 
 
@@ -21,14 +22,16 @@ function showPosition(position) {
   
   // Check console
   console.log("Device Location Latitude: " + lat + " Longitude: " + lon);
-  console.log(`Devicer location latitude: ${lat} Longitude: ${lon}`);
+  console.log(`Device location latitude: ${lat} Longitude: ${lon}`);
 }
 
 
 $(document).ready(function() {
 
+  // may not need this current date var anymore, since we changed the API calls we're using
   var momentTodayDate = moment().format("YYYY-MM-DD")
 
+  // still need to convert this ISO date to local deviceDateTime
   var deviceDateTime = new Date().toISOString()
   
   // string interpolation for the device date time 
@@ -59,6 +62,7 @@ $(document).ready(function() {
     console.log("filmsNowShowing");
     console.log(response);
     filmsBody.empty();
+    // runs the for loop function immediately below (which will output top 10 films with showings)
     filmsNowShowing(response);
   });
 
@@ -69,13 +73,6 @@ $(document).ready(function() {
     let filmId = response.films[i].film_id;
     let filmName = response.films[i].film_name;
     let filmImage = response.films[i].images.poster["1"].medium.film_image;
-
-    // let cityDate = moment().format('l');
-    // let cityIcon = response.weather[0].icon;
-    // let cityTemp = Math.round(response.main.temp);
-    // let cityHumid = response.main.humidity;
-    // let cityWind = Math.round(response.wind.speed);
-    // let cityIconEl = $("<img>").attr("src", `https://openweathermap.org/img/w/${cityIcon}.png`)
 
     let cardEl = $("<div>").attr("class", "card movieCard");
     let cardBodyEl = $("<div>").attr("class", "card-body ten-card");
@@ -89,17 +86,66 @@ $(document).ready(function() {
     $("#ten-card-deck").append(cardEl);
   };
 
-//Put the filmshowtimes ajax call function within the image onclick event. This way it only runs after clicking specific image, grabbing that film id data attribute as part of the call. 
+//Put the closestShowings ajax call function within function "filmsNowsShowing" and image onclick event. This way it only runs after clicking specific image, grabbing that film id data attribute as part of the call. 
 $("img").on("click", function() {
   
   // film Id info is pulled from the data attribute set above on cardImageEl
   var filmIdData = $(this).data("film");
   var filmNameData = $(this).data("name");
   var cinemaBody = $("#cinemaOutput");
+  var omdbBody = $("#movieInfo")
 
   console.log(filmIdData);
   console.log(filmNameData);
 
+  // Movie info URL call
+  var omdbQueryURL = "https://www.omdbapi.com/?t=" + filmNameData + "&apikey=trilogy";
+  
+  $.ajax({
+    url: omdbQueryURL,
+    method: "GET"
+  }).then(function(response) {
+
+    console.log ("movieDetails");
+    console.log (response);
+    omdbBody.empty();
+    movieDetails(response);
+  });
+  
+  function movieDetails(response) {
+
+    // defining the object data for title, release date, rated, runtime, genre, cast, Director, Synopsis, ratings
+
+    let title = response.Title;
+    // format is 22 Nov 2019 (should convert to November 22, 2019)
+    let released = moment(response.Released, "DD MMM YYYY").format("MMMM DD, YYYY");
+    let rated = response.Rated;
+    let runtime = response.Runtime;
+    let genre = response.Genre;
+    let cast = response.Actors;
+    let director = response.Director;
+    let plot = response.Plot
+    let imdb = response.imdbRating;
+    let metascore = response.Metascore;
+
+    // Create divs, header, and p tags for each object data
+    let detailsCardEl = $("<div>").attr("class", "card detailsCard");
+    let detailsCardBodyEl = $("<div>").attr("class", "card-body");
+    let titleEl = $("<h6>").attr("class", "card-title").text(title);
+    let releasedEl = $("<p>").attr("class", "released").text("Released: " + released);
+    let ratedEl = $("<p>").attr("class", "rated").text("Rated: " + rated);
+    let runtimeEl = $("<p>").attr("class", "runtime").text("Runtime: " + runtime);
+    let genreEl = $("<p>").attr("class", "genre").text("Genre: " + genre);
+    let castEl = $("<p>").attr("class", "cast").text("Cast: " + cast);
+    let directorEl = $("<p>").attr("class", "director").text("Director: " + director) 
+    let plotEl = $("<p>").attr("class", "plot").text("Plot: " + plot) 
+    let ratingsEl = $("<p>").attr("class", "ratings").text("IMDB: " + imdb + "  " + "Metascore: " + metascore);
+
+    // appended all the elements created
+    detailsCardEl.append(detailsCardBodyEl);
+    detailsCardBodyEl.append(titleEl, releasedEl, ratedEl, runtimeEl, genreEl, castEl, directorEl, plotEl, ratingsEl)
+    $("#movieInfo").append(detailsCardEl);
+  };
 
 
  // Cinema Show Times API Settings 
@@ -109,11 +155,12 @@ $("img").on("click", function() {
   "method": "GET",
   "headers": {
     "api-version": "v200",
-    "Authorization": "Basic Qk9PVF8yOmdBSkVFWFhuTFlHVw===",
-    "client": "BOOT_2",
-    "x-api-key": "ZgTYUEfaLc3nwZHwGLzaD5kwBH0fGAgZ7eHIL724",
+    "Authorization": "Basic TlpEWjpxbXV5SWFST0RTbVk=",
+    "client": "NZDZ",
+    "x-api-key": "E2PqsAEK8R2SyYgdJTBhx5gCCxuE4oMb3sYkjZoK",
     "device-datetime": `${deviceDateTime}`,
     "territory": "US",
+    // We still need to replace this geolocation with a var from geolocation function at the top (Yong?)
     "Geolocation": "42.0446208;-87.6675072",
     }
   }
@@ -124,6 +171,7 @@ $("img").on("click", function() {
     console.log("closestShowing");
     console.log(response);
     cinemaBody.empty();
+    // runs the for loop function immediately below (which will output nearest 5 cinema details)
     closestShowing(response);
 
   });
@@ -132,65 +180,58 @@ $("img").on("click", function() {
 
     for (let i = 0; i < 5; i ++) {
 
+    // Use for loop to generate all (5) cinema information with the next available showing data. 
     let filmId = response.film_id;
+    let filmName = response.film_name;
     let cinemaId = response.cinemas[i].cinema_id;
     let cinemaName = response.cinemas[i].cinema_name;
     let cinemaAddress = response.cinemas[i].address;
     let cinemaCity = response.cinemas[i].city;
     let cinemaPostcode = response.cinemas[i].postcode;
-    let cinemaDistance = response.cinemas[i].distance;
-    let cinemaNextShow = response.cinemas[i].time;
+    let cinemaDistance = parseFloat(response.cinemas[i].distance).toFixed(2);
+    let showingDate = moment(response.cinemas[i].date, "YYYY-MM-DD").format("MMM D")
+    let cinemaNextShow = moment(response.cinemas[i].time, "HH:mm").format("h:mm A");
     let cinemaLat = response.cinemas[i].lat;
     let cinemaLng = response.cinemas[i].lng;
 
+    // Create divs, header, and p tags for each object data
     let cinemaCardEl = $("<div>").attr("class", "card cinemaCard");
     let cinemaCardBodyEl = $("<div>").attr("class", "card-body five-card");
     let cinemaNameEl = $("<h6>").attr("class", "card-title").text(cinemaName);
     cinemaCardEl.attr("data-cinema", cinemaId);
     cinemaCardEl.attr("data-film", filmId);
+    // Yong, you would need to create data attributes for your cinemaLat and cinemaLng lets like the lines immediately above, in order to pull that data with an onclick event
+    // cinemaCardEl.attr("data-lat", cinemaLat);
+    // cinemaCardEl.attr("data-lng", cinemaLng);
     let cinemaAddressEl = $("<p>").attr("class", "address").text(cinemaAddress);
     let cinemaCityEl = $("<p>").attr("class", "city").text(cinemaCity);
     let cinemaPostcodeEl = $("<p>").attr("class", "postcode").text(cinemaPostcode);
-    let cinemaDistanceEl = $("<p>").attr("class", "distance").text(cinemaDistance);
-    let cinemaNextShowEl = $("<p>").attr("class", "nextShow").text(cinemaNextShow);
+    let cinemaDistanceEl = $("<p>").attr("class", "distance").text(cinemaDistance + " miles away");
+    let filmNameEl = $("<p>").attr("class", "filmName").text(filmName);
+    // materialize badges didn't work as is, even though correct format and classes added. Will look into or eventually remove
+    let cinemaNextShowEl = $("<p>").attr("class", "nextShow new badge").text(showingDate + " " + cinemaNextShow);
+    cinemaNextShowEl.attr("data-badge-caption", "Showing")
 
 
-
+    // append all the elements created 
     cinemaCardEl.append(cinemaCardBodyEl);
-    cinemaCardBodyEl.append(cinemaNameEl, cinemaAddressEl, cinemaCityEl, cinemaPostcodeEl, cinemaDistanceEl, cinemaNextShowEl)
+    cinemaCardBodyEl.append(cinemaNameEl, cinemaAddressEl, cinemaCityEl, cinemaPostcodeEl, cinemaDistanceEl, filmNameEl, cinemaNextShowEl)
     $("#cinemaOutput").append(cinemaCardEl);
   };
+
+ // Yong, here's where I believe you would put the .cinemaCard onclick event function like I have above for the img tag function. Define the data attributes you'll be using/pulling from the data attributes you created above for cinemaLat and cinemaLng, like below
+  //  var latData = $(this).data("lat");
+  //  var lngData = $(this).data("lng");
+
+    //Within this onclick function, you would need to put the google maps API call. This way it only runs after clicking specific cinema, grabbing that cinema lat/lng data attribute as part of the call. Make sure to also add a .empty() function so that if you click on a different cinema, the new map will replace the old map.
+
+  
   };
 
 
 });
 
 };
-
-// no longer using cinemaDetails API, commenting out
-//   // var cinemaDetailsSetting;
-//   // Cinema Details API Settings
-//   var cinemaDetailsSetting = {
-//     "crossDomain": true,
-//     "url": "https://api-gate2.movieglu.com/cinemaDetails/?cinema_id=7607",
-//     "method": "GET",
-//     "headers": {
-//       "api-version": "v200",
-//       "Authorization": "Basic U01JVF8wOkF4emkwWEppMDFlcg==",
-//       "client": "SMIT_0",
-//       "x-api-key": "ZRUjCrZ5r18epZovOj1A3aesuvedkfbZ7Dy06U7U",
-//       "device-datetime": `${deviceDateTime}`,
-//       "territory": "US",
-  
-//     }
-//   }
-
-// $.ajax(cinemaDetailsSetting).done(function (response) {
-//   // cinemaDetails(response);
-//   console.log("cinemaDetails");
-//   console.log(response);
-
-//   });
   
 
 
